1046c011980f9759899ea6515dd63fad874598b5
kirill_v <volik_kirill@eclipso.eu>
2021-02-24 19:52:12 +0100

    Changing input.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
+from input_ import UserInput
 
 def main():
-    pass
+    """Main function of the program."""
+    url = UserInput()
+    print(f'You entered next URL: {url}')
+
+    
 
 if __name__ == "__main__":
     main()
