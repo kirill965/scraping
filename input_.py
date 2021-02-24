8b1eae8dfa2c16c8474c7c0cb827bec41915d736
kirill_v <volik_kirill@eclipso.eu>
2021-02-24 19:50:14 +0100

    Adding using input function.

---
+++ input_.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def UserInput():
+    """Function which takes an URL from user and return it as string."""
+    print('Enter your URL here: ')
+    url_ = input()
+    return url_
+
+if __name__ == '__main__':
+    UserInput()
